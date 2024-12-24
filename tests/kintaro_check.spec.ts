46ca69a7c2a813c0d2f5dc3bbd1a164d89d48627
hideaki.murakami <hideaki.murakami@nijibox.co.jp>
2024-12-24 16:53:43 +0900

    リファクタ

--- tests/kintaro_check.spec.ts
+++ tests/kintaro_check.spec.ts
@@ -39,7 +39,7 @@ test('kintaro_check', async () => {
   await page.getByRole('link', { name: '日次データ出力' }).click();
   await page.getByLabel('月度指定').check();
 
-  // 今月分のデータを選択
+  // csv取得:今月分のデータを選択
   const inputLocator = page.locator('input[name="YearMonth"]').nth(1);
   const inputValue = await inputLocator.getAttribute('value');
   const currentYM = current.getFullYear() + "/" + (current.getMonth() + 1);
@@ -47,7 +47,7 @@ test('kintaro_check', async () => {
     await page.locator('#pickernext').click();
   }
 
-  // ダウンロード操作
+  // csvダウンロード操作
   await page.locator('#modal-WC210-select-btn').click();
   await page.locator('#modal-WC210 thead').getByRole('checkbox').check();
   await page.getByRole('button', { name: 'OK' }).click();
@@ -55,7 +55,7 @@ test('kintaro_check', async () => {
   const download = await page.waitForEvent('download');
   await download.saveAs(path.join('./settings', download.suggestedFilename()));
 
-  // 勤怠入寮状況の一覧をDOMから取得
+  // 勤怠入力状況の一覧をDOMから取得
   // ※未入力判定はDOM上を調べたほうが確実
   // ※csvには勤怠未入力日と休日両方が混ざっており、休日判定が大変
   await page.getByRole('link', { name: '承認状況一覧' }).click();
@@ -70,13 +70,8 @@ test('kintaro_check', async () => {
   // クラス名が "kinmudatalist" の中の最初のtableタグを取得
   // ※2つ目のtableタグは列固定用のDOM
   const contentHandle = page.locator('.kinmudatalist > table').first();
-
-  // チェックする日付
   const checkDate = current.getDate() - 1;
-  // const checkDate = 31; // デバッグ用
-
-  // 稼働状況をDOMから解析
-  const checkResult = await checkAttendance(checkDate, contentHandle);
+  const checkResult = await checkAttendance(contentHandle, checkDate);
   const checkResultString = formatSlackStr(checkResult);
 
   // =====================
@@ -114,61 +109,59 @@ test('kintaro_check', async () => {
 });
 
 // 稼働状況をDOMから解析（playwrightメソッドを使用しているのでここに置いている）
-const checkAttendance = async (checkDate: number, contentHandle: Locator) => {
+const checkAttendance = async (contentHandle: Locator, checkDate: number) => {
   const memberData: MemberData[] = [];
+  let notInputList: string[] = [];
   let memberCnt = 0;
 
-  if (contentHandle) {
-    const cellsLocator = contentHandle.locator('td[data-colidx]:not([data-colidx="0"], [data-colidx="1"])');
-    const cellCount = await cellsLocator.count();
+  if (!contentHandle) return memberData;
 
-    let notInputList: string[] = [];
+  const cellsLocator = contentHandle.locator('td[data-colidx]:not([data-colidx="0"], [data-colidx="1"])');
+  const cellCount = await cellsLocator.count();
 
-    for (let i = 0; i < cellCount; i++) {
-      const cellLocator = cellsLocator.nth(i);
-      // data-colidx の取得
-      const colIdx = await cellLocator.getAttribute("data-colidx");
-      const colCnt = Number(colIdx) - 2;
+  for (let i = 0; i < cellCount; i++) {
+    const cellLocator = cellsLocator.nth(i);
+    // data-colidx の取得
+    const colIdx = await cellLocator.getAttribute("data-colidx");
+    const colCnt = Number(colIdx) - 2;
 
-      // 今日以降の日付チェックはスキップする
-      if (checkDate < colCnt) continue;
+    // 今日以降の日付チェックはスキップする
+    if (checkDate < colCnt) continue;
 
-      // memberData[memberCnt]が存在しない場合に新しい要素を追加
-      if (!memberData[memberCnt]) {
-        memberData[memberCnt] = { name: "", date: [] };
-      }
+    // memberData[memberCnt]が存在しない場合に新しい要素を追加
+    if (!memberData[memberCnt]) {
+      memberData[memberCnt] = { name: "", date: [] };
+    }
 
-      // テキスト内容の取得
-      const text = await cellLocator.textContent();
+    // テキスト内容の取得
+    const text = await cellLocator.textContent();
 
-      // 氏名列の処理
-      if (colIdx === "2") {
-        console.log('[checkAttendance] =====================================');
-        console.log('[checkAttendance] 氏名:', text?.trim());
+    // 氏名列の処理
+    if (colIdx === "2") {
+      console.log('[checkAttendance] =====================================');
+      console.log('[checkAttendance] 氏名:', text?.trim());
 
-        memberData[memberCnt].name = text?.trim() || '';
-        notInputList = [];
-        continue;
-      }
+      memberData[memberCnt].name = text?.trim() || '';
+      notInputList = []; // メンバーごとに初期化
+      continue;
+    }
 
-      // 日付列の処理
-      const classes = await cellLocator.evaluate((node) =>
-        Array.from((node as Element).classList)
-      );
-      const notInputWorkDay = classes.filter(
-        () => !classes.includes("offday") && classes.includes("jsk-0")
-      );
+    // 日付列の処理:休日（"offday"）ではない未入力日（"jsk-0"）を抽出
+    const classes = await cellLocator.evaluate((node) =>
+      Array.from((node as Element).classList)
+    );
+    const notInputWorkDay = classes.filter(
+      () => !classes.includes("offday") && classes.includes("jsk-0")
+    );
+    if (notInputWorkDay.length > 0) {
+      notInputList.push(`${colCnt}日`);
+    }
 
-      if (notInputWorkDay.length > 0) {
-        notInputList.push(`${colCnt}日`);
-      }
-
-      // 最終日の集計が終わったところで出力
-      if (colCnt === checkDate) {
-        console.log(`[checkAttendance] ${notInputList}`);
-        memberData[memberCnt].date = notInputList;
-        memberCnt++;
-      }
+    // チェック最終日の集計が終わったところで出力
+    if (colCnt === checkDate) {
+      console.log(`[checkAttendance] ${notInputList}`);
+      memberData[memberCnt].date = notInputList;
+      memberCnt++;
     }
   }
 
