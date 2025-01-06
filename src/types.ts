4118eb917b3f7ee3b47ab5b5cfa73b6c8c37c51f
hideaki.murakami <hideaki.murakami@nijibox.co.jp>
2025-01-06 12:27:18 +0900

    型ファイルをリファクタ

--- src/types.ts
+++ src/types.ts
@@ -1,40 +1,41 @@
-export type PostParts = {
+type PostParts = {
   type: string;
   text: string;
 };
 
+// slack投稿テキスト
 export type PostItem = {
   type: string;
   text: PostParts;
 };
 
+// 投稿チャンネル + slack投稿テキスト
 export type SlackTextPayload = {
   channel: string | null;
   blocks: PostItem[];
 };
 
+// 勤怠状況DOM解析結果
 export type CheckResult = {
   name: string;
   date: string[];
 };
 
+// csv チャンネル設定格納用
+export type ChannelsData = {
+  channelId: string;
+  group: string;
+};
+
+// csv メンション設定格納用
 export type MentionsData = {
   group: string;
   kintaroName: string;
   slackUserID: string;
 };
 
+// csv 勤怠データ（実績かい離分析）格納用
 export type DeviationData = {
   [key: string]: string[];
 };
 
-export type ChannelsData = {
-  channelId: string;
-  group: string;
-};
-
-export type PostResult = {
-  resultSuccess: string[];
-  resultError: string[];
-  resultErrorReason: string[];
-};
